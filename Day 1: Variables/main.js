8b47072cacd16c65be5080665a8886fb1d2499d6
JulianSellanes <julian.sellanes@gmail.com>
2023-10-12 14:45:39 -0300

    #1

---
+++ Day 1: Variables/main.js
@@ -0,0 +1,39 @@
+console.log("Hello World");
+
+let integer = 4;
+let float = 6.7;
+let string = `string`;
+let boolean = false;
+let undefined;
+let empty = null;
+
+/*
+console.log(integer);
+console.log(float);
+console.log(string);
+console.log(boolean);
+console.log(undefined);
+console.log(empty);
+*/
+
+let fistName = `Noa`;
+let lastName = `Allen`;
+let country = `Canada`;
+let age = 20;
+let isLearning = true;
+const gravity = 9.81;
+const PI = 3.14
+
+/*
+console.log(fistName);
+console.log(lastName);
+console.log(country);
+console.log(age);
+console.log(isLearning);
+console.log(gravity);
+console.log(PI);
+*/
+
+let a = 1, b = 3.5, c = `Hi`;
+
+console.log(a, b, c);
