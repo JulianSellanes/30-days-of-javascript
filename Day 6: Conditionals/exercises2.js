ce6d0c2c393f67d630982a9250faaf17df7a39fb
JulianSellanes <julian.sellanes@gmail.com>
2023-10-17 12:35:53 -0300

    #6

---
+++ Day 6: Conditionals/exercises2.js
@@ -0,0 +1,73 @@
+// Write a code which can give grades to students according to theirs scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+
+/*
+let score = parseInt(prompt(`Enter score:`));
+
+if(score >= 80)
+{
+    console.log(`Grade: A`);
+}
+else if(score >= 70)
+{
+    console.log(`Grade: B`);
+}
+else if(score >= 60)
+{
+    console.log(`Grade: C`);
+}
+else if(score >= 50)
+{
+    console.log(`Grade: D`);
+}
+else
+{
+    console.log(`Grade: F`);
+}
+*/
+
+// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+/*
+let month = prompt(`Enter a month:`).toLowerCase();
+
+if(month === `september` || month === `october` || month === `november`)
+{
+    console.log(`The season is Autumn`);
+}
+else if(month === `december` || month === `january` || month === `february`)
+{
+    console.log(`The season is Winter`);
+}
+else if(month === `march` || month === `april` || month === `may`)
+{
+    console.log(`The season is Spring`);
+}
+else if(month === `june` || month === `july` || month === `august`)
+{
+    console.log(`The season is Summer`);
+}
+*/
+
+// Check if a day is weekend day or a working day. Your script will take day as an input.
+
+/*
+let day = prompt(`Enter day:`).toLowerCase();
+
+if(day === `saturday` || day === `sunday`)
+{
+    console.log(`${day} is a weekend`);
+}
+else
+{
+    console.log(`${day} is a working day`);
+}
+*/
