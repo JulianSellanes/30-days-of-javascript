955bdd75356d9be2b884681cc456ff189244b343
JulianSellanes <julian.sellanes@gmail.com>
2023-11-01 18:07:21 -0300

    #12

---
+++ Day 12: Sets/exercises1.js
@@ -0,0 +1,55 @@
+const countries = ['Finland', 'Sweden', 'Norway']
+
+// Create an empty set
+
+/*
+const set = new Set();
+console.log(set);
+*/
+
+// Create a set containing 0 to 10 using loop
+
+/*
+const set = new Set();
+for(let i = 0; i <= 10; i++)
+{
+    set.add(i);
+}
+console.log(set);
+*/
+
+// Remove an element from a set
+
+/*
+const array = [1, 2, 3, 4];
+const set = new Set(array);
+set.delete(1);
+console.log(set);
+*/
+
+// Clear a set
+
+/*
+const array = [1, 2, 3, 4];
+const set = new Set(array);
+set.clear();
+console.log(set);
+*/
+
+// Create a set of 5 string elements from array
+
+/*
+const array = [`1`, `2`, `3`, `4`, `5`];
+const set = new Set(array);
+console.log(set);
+*/
+
+// Create a map of countries and number of characters of a country
+
+/*
+const map = new Map();
+countries.forEach(item => {
+    map.set(item, item.length);
+});
+console.log(map);
+*/
