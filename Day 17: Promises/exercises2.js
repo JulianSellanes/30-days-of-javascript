955d2342d0b83566acabd7cc906451c934dbb21d
JulianSellanes <julian.sellanes@gmail.com>
2023-11-10 12:56:14 -0300

    #17

---
+++ Day 17: Promises/exercises2.js
@@ -0,0 +1,14 @@
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+// Print out all the cat names in to catNames variable.
+
+/*
+fetch(catsAPI)
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(item => {
+            console.log(`${item.name}`);
+        });
+    })
+    .catch(error => console.log(error));
+*/
