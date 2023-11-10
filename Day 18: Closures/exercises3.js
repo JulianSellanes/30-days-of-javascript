7a5196656ddcfb1e0bc2fa109c685c7d8a8eec2b
JulianSellanes <julian.sellanes@gmail.com>
2023-11-10 17:33:05 -0300

    #18

---
+++ Day 18: Closures/exercises3.js
@@ -0,0 +1,39 @@
+// Create a personAccount out function. It has firstname, lastname, incomes, expenses inner variables.
+// It has totalIncome, totalExpense, accountInfo, addIncome, addExpense and accountBalance inner functions.
+// Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+/*
+const firstName = `Noa`;
+const lastName = `Allen`;
+const incomes = [1, 2, 3];
+const expenses = [9, 8, 7];
+
+const personAccountOuter = () =>
+{
+    function totalIncome()
+    {
+        return incomes;
+    }
+
+    function totalExpense()
+    {
+        return expenses;
+    }
+
+    function accountInfo()
+    {
+        return firstName + ` ` + lastName;
+    }
+    
+    return {
+        totalIncome:totalIncome(),
+        totalExpense:totalExpense(),
+        accountInfo:accountInfo()
+    }
+}
+const inner = personAccountOuter();
+
+console.log(inner.totalIncome);
+console.log(inner.totalExpense);
+console.log(inner.accountInfo);
+*/
