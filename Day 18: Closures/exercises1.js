aef9bca8456f1629be828c06c5243f808f5d788b
JulianSellanes <julian.sellanes@gmail.com>
2023-11-10 17:33:01 -0300

    #18

---
+++ Day 18: Closures/exercises1.js
@@ -0,0 +1,17 @@
+// Create a closure which has one inner function
+
+/*
+const outer = () =>
+{
+    let count = 0;
+    const inner = () =>
+    {
+        count += 2;
+        return count;
+    }
+
+    return inner();
+}
+
+console.log(outer());
+*/
